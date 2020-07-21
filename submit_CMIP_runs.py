4f879f548d3a1fd1ed0bb8f9effbab79a52cbaee
antonia-had <ah986@cornell.edu>
2020-07-21 14:04:48 -0400

    minor

--- submit_CMIP_runs.py
+++ submit_CMIP_runs.py
@@ -26,14 +26,14 @@ if rank < remainder:
 else:
     start = remainder * (count + 1) + (rank - remainder) * count
     stop = start + count
-
+print(rank)
 # =============================================================================
 # Go though all scenarios
 # =============================================================================
 for k in range(start, stop):
-    # Change to scenario subdirectory
-    os.chdir('./CO_climate_scenarios/'+directories[k]+'/cm2015/StateMod/')
-    # Create symbolic link to statemod executable
-    os.system("ln -s /home/fs02/pmr82_0001/ah986/Colorado/cdss-app-statemod-fortran/src/main/fortran/statemod-15.00.14dev-gfortran-32bit .")
-    # Run simulation
-    os.system("./statemod-15.00.14dev-gfortran-32bit cm2015B -simulate")
+    # # Change to scenario subdirectory
+    # os.chdir('/scratch/ah986/rival_framings_demand/CO_climate_scenarios/'+directories[k]+'/cm2015/StateMod/')
+    # # Create symbolic link to statemod executable
+    # os.system("ln -s /home/fs02/pmr82_0001/ah986/Colorado/cdss-app-statemod-fortran/src/main/fortran/statemod-15.00.14dev-gfortran-32bit .")
+    # # Run simulation
+    # os.system("./statemod-15.00.14dev-gfortran-32bit cm2015B -simulate")
