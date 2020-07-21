832d844c5f54d4078b093d4bf2808bca18c9a1cc
antonia-had <ah986@cornell.edu>
2020-07-21 11:45:56 -0400

    initialize and submit CMIP runs

---
+++ submit_CMIP_runs.py
@@ -0,0 +1,39 @@
+from mpi4py import MPI
+import math
+import os
+
+directories = os.listdir()
+scenarios = len(directories)
+# =============================================================================
+# Start parallelization
+# =============================================================================
+
+# Begin parallel simulation
+comm = MPI.COMM_WORLD
+
+# Get the number of processors and the rank of processors
+rank = comm.rank
+nprocs = comm.size
+
+# Determine the chunk which each processor will need to do
+count = int(math.floor(scenarios / nprocs))
+remainder = scenarios % nprocs
+
+# Use the processor rank to determine the chunk of work each processor will do
+if rank < remainder:
+    start = rank * (count + 1)
+    stop = start + count + 1
+else:
+    start = remainder * (count + 1) + (rank - remainder) * count
+    stop = start + count
+
+# =============================================================================
+# Go though all scenarios
+# =============================================================================
+for k in range(start, stop):
+    # Change to scenario subdirectory
+    os.chdir('/'+directories[k]+'/cm2015/StateMod/')
+    # Create symbolic link to statemod executable
+    os.system("ln -s /home/fs02/pmr82_0001/ah986/Colorado/cdss-app-statemod-fortran/src/main/fortran/statemod-15.00.14dev-gfortran-32bit .")
+    # Run simulation
+    os.system("./statemod-15.00.14dev-gfortran-32bit cm2015B -simulate")
