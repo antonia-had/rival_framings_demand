893fa023bf4548c8ebf304d0a578aafd1e2b58b1
antonia-had <ah986@cornell.edu>
2020-07-30 14:10:50 -0400

    changed statemod version

--- submit_CMIP_runs.py
+++ submit_CMIP_runs.py
@@ -34,8 +34,8 @@ for k in range(start, stop):
     # Change to scenario subdirectory
     os.chdir('/scratch/ah986/rival_framings_demand/CO_climate_scenarios/'+directories[k]+'/cm2015/StateMod/')
     # # Create symbolic link to statemod executable if not already there
-    # if not os.path.isfile("./statemod-15.00.14dev-gfortran-32bit"):
-    #     os.system("ln -s /home/fs02/pmr82_0001/ah986/Colorado/cdss-app-statemod-fortran/src/main/fortran/statemod-15.00.14dev-gfortran-32bit .")
+    # if not os.path.isfile("./statemod"):
+    #     os.system("ln -s /home/fs02/pmr82_0001/ah986/Colorado/yates_statemod/src/src/main/fortran/statemod .")
     # Run simulation
-    os.system("./statemod-15.00.14dev-gfortran-32bit cm2015B -simulate")
+    os.system("./statemod cm2015B -simulate")
     print ('simulating '+ directories[k])
