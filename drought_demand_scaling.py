c5105fec3f7639b5176c0694da3f2093a443c604
antonia-had <ah986@cornell.edu>
2020-08-27 20:59:22 -0400

    test writing to IWR

--- drought_demand_scaling.py
+++ drought_demand_scaling.py
@@ -1,21 +1,46 @@
 import os
 import pyDOE2
-from utils import search_string_in_file
 import numpy as np
-
+import pandas as pd
+from utils import writenewIWR
 
 directories = os.listdir('./CMIP_curtailment')
 scenarios = len(directories)
 
-shortage_levels = [10, 20, 30]
-no_users = [2, 5, 10]
-curtailment = [20, 50, 100]
-duration = [1, 2, 5]
+hist_iwr = np.loadtxt('./hist_files/MonthlyIWR.csv', delimiter=',')
+hist_flows = np.loadtxt('./hist_files/MonthlyFlows.csv', delimiter=',')
+annual_flows = np.sum(hist_flows, axis=1)
+hist_shortages = np.loadtxt('./hist_files/annual_shortages.csv', delimiter=',')
+rights = pd.read_csv('./hist_files/iwr_admin.csv')
 
-sample = pyDOE2.fullfact([3, 3, 3, 3])
+shortage_percentile = [100, 95, 90]
+no_users = [2, 5, 10]
+curtailment_levels = [20, 50, 100]
+
+sample = pyDOE2.fullfact([3, 3, 3])
 
 numSites = 379
 
-monthly_iwr = np.loadtxt('./CMIP_scenarios/' + scenario + '/MonthlyIWR.csv', delimiter=',')
-monthly_flows = np.loadtxt('./CMIP_scenarios/' + scenario + '/MonthlyFlows.csv', delimiter=',')
+# Use historical shortages and flows to determine trigger flows for curtailment
+trigger_years = [abs(hist_shortages-np.percentile(hist_shortages, p, interpolation='nearest')).argmin() for p in shortage_percentile]
+trigger_flows = [annual_flows[t] for t in trigger_years]
 
+# Use rights to determine users to curtail
+threshold_admins = [np.percentile(rights['Admin'].values, 100-p, interpolation='nearest') for p in no_users]
+users_to_curtail = [rights.loc[rights['Admin'] > t]['WDID'].values for t in threshold_admins]
+
+listofyears = np.arange(1950,2013)
+
+for scenario in directories[:1]:
+    monthly_flows = np.loadtxt('./CMIP_scenarios/' + scenario + '/MonthlyFlows.csv', delimiter=',')
+    annual_flows_scenario = np.sum(monthly_flows, axis=1)
+    # Apply each sample to every CMIP scenario
+    for i in range(len(3)):#sample[:,0])):
+        trigger_flow = trigger_flows[sample[i,0]]
+        users = users_to_curtail[sample[i,1]]
+        curtailment = curtailment_levels[sample[i,2]]
+
+        low_flows = annual_flows <= trigger_flow
+        curtailment_years = np.arange(1950,2014)[low_flows]
+
+        writenewIWR(scenario, 'cm2015B.iwr', 463, i, users, curtailment, curtailment_years)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def search_string_in_file(file_name, string_to_search):
     """Search for the given string in file and return the line numbers containing that string"""
     line_number = 0
@@ -13,3 +15,50 @@ def search_string_in_file(file_name, string_to_search):
                 list_of_results.append((line_number))
     # Return list of tuples containing line numbers and lines where string is found
     return list_of_results
+
+
+def writenewIWR(directory, filename, firstLine, i, users, curtailment, curtailment_years):
+    # split data on periods
+    with open(filename, 'r') as f:
+        all_split_data = [x.split('.') for x in f.readlines()]
+    f.close()
+
+    # get unsplit data to rewrite firstLine # of rows
+    with open(filename, 'r') as f:
+        all_data = [x for x in f.readlines()]
+    f.close()
+
+    # replace former iwr demands with new
+    new_data = []
+    for i in range(len(all_split_data) - firstLine):
+        row_data = []
+        # split first 3 columns of row on space and find 1st month's flow
+        row_data.extend(all_split_data[i + firstLine][0].split())
+        # check if year is a curtailment year
+        if row_data[0] in curtailment_years:
+            # check if user is to be curtailed
+            if row_data[1] in users:
+                #scale first month
+                row_data[2] = str(int(float(row_data[2])*(100-curtailment)/100))
+                #scale other months
+                for j in range(len(all_split_data[i + firstLine]) - 2):
+                    row_data.append(str(int(float(all_split_data[i + firstLine][j + 1]) *(100-curtailment)/100)))
+        # append row of adjusted data
+        new_data.append(row_data)
+
+    f = open('./CMIP_scenarios/' + directory + '/cm2015/StateMod/'+ filename[0:-4] + '_S' + str(i) + filename[-4::], 'w')
+    # write firstLine # of rows as in initial file
+    for i in range(firstLine):
+        f.write(all_data[i])
+    for i in range(len(new_data)):
+        # write year, ID and first month of adjusted data
+        f.write(new_data[i][0] + ' ' + new_data[i][1] + (19 - len(new_data[i][1]) - len(new_data[i][2])) * ' ' +
+                new_data[i][2] + '.')
+        # write all but last month of adjusted data
+        for j in range(len(new_data[i]) - 4):
+            f.write((7 - len(new_data[i][j + 3])) * ' ' + new_data[i][j + 3] + '.')
+        # write last month of adjusted data
+        f.write((9 - len(new_data[i][-1])) * ' ' + new_data[i][-1] + '.' + '\n')
+    f.close()
+
+    return None
