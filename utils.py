2f9fd9f549c8a6697fcaa7bf744c26235a4c9101
antonia-had <ah986@cornell.edu>
2020-08-31 18:44:59 -0400

    debug

--- utils.py
+++ utils.py
@@ -36,6 +36,7 @@ def writenewIWR(directory, filename, sample, users, curtailment_per_user, genera
         row_data = []
         # split first 3 columns of row on space and find 1st month's flow
         row_data.extend(all_split_data[i + firstLine][0].split())
+        print(row_data)
         # check if year is a curtailment year and if user is to be curtailed
         if row_data[0] in curtailment_years and row_data[1] in users:
             index = np.where(users==row_data[1])
@@ -48,7 +49,7 @@ def writenewIWR(directory, filename, sample, users, curtailment_per_user, genera
         # append row of adjusted data
         new_data.append(row_data)
 
-    f = open('./CMIP_curtailment/' + directory + '/cm2015/StateMod/' + filename[0:-4] + '_S' + str(i) + filename[-4::], 'w')
+    f = open('./CMIP_curtailment/' + directory + '/cm2015/StateMod/' + filename[0:-4] + '_S' + str(sample) + filename[-4::], 'w')
     # write firstLine # of rows as in initial file
     for i in range(firstLine):
         f.write(all_data[i])
