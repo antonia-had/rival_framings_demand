2a38b003a25e5d533897c069c5e924b9b2467f38
antonia-had <ah986@cornell.edu>
2020-08-31 20:59:07 -0400

    debug

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def writenewIWR(directory, filename, sample, users, curtailment_per_user, genera
             #scale first month
             row_data[2] = str(int(float(row_data[2])*remaining_demand))
             #scale other months
-            print(str(len(all_split_data[i + firstLine]) - 2))
+            print(all_split_data[i + firstLine])
             for j in range(len(all_split_data[i + firstLine]) - 2):
                 row_data.append(str(int(float(all_split_data[i + firstLine][j + 1]) * remaining_demand)))
         # append row of adjusted data
