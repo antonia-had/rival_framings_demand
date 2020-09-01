990070de4867a569142449457228f546fd4131d7
antonia-had <ah986@cornell.edu>
2020-09-01 14:37:25 -0400

    env source

--- submit_CMIP_runs.sh
+++ submit_CMIP_runs.sh
@@ -7,5 +7,6 @@
 #SBATCH -t 1:00:00            # set max wallclock time
 
 module load python/3.6.9
+source /home/fs02/pmr82_0001/ah986/envs/rival_framings/bin/activate
 mpirun python3 submit_CMIP_runs.py
 
