6fab62a14f27993e5755690248bc2436256e4718
antonia-had <ah986@cornell.edu>
2020-08-31 22:01:54 -0400

    debug

--- utils.py
+++ utils.py
@@ -39,6 +39,7 @@ def writenewIWR(directory, filename, sample, users, curtailment_per_user, genera
         # check if year is a curtailment year and if user is to be curtailed
         if int(row_data[0]) in curtailment_years and row_data[1] in users:
             print(row_data[1])
+            print(np.where(users==row_data[1]))
             index = np.where(users==row_data[1])[0][0]
             remaining_demand = 1-(curtailment_per_user[index]*(100-general_curtailment)/100)
             #scale first month
