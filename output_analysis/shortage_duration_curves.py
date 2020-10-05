502b8f054b1808db520d1befe32898a8286fae28
antonia-had <ah986@cornell.edu>
2020-10-05 17:53:57 -0400

    add historical curve

--- output_analysis/shortage_duration_curves.py
+++ output_analysis/shortage_duration_curves.py
@@ -46,18 +46,18 @@ def shortage_duration(sequence, threshold):
                 key]  # Counts groups of True values
     return shrt_dur
 
-def plotSDC(synthetic, structure_name):
+def plotSDC(synthetic, histData,  structure_name):
     n = 12
-    # # Reshape historic data to a [no. years x no. months] matrix
-    # f_hist = np.reshape(histData, (int(np.size(histData) / n), n))
-    # # Reshape to annual totals
-    # f_hist_totals = np.sum(f_hist, 1)
-    # # Calculate historical shortage duration curves
-    # F_hist = np.sort(f_hist_totals)  # for inverse sorting add this at the end [::-1]
+    # Reshape historic data to a [no. years x no. months] matrix
+    f_hist = np.reshape(histData, (int(np.size(histData) / n), n))
+    # Reshape to annual totals
+    f_hist_totals = np.sum(f_hist, 1)
+    # Calculate historical shortage duration curves
+    F_hist = np.sort(f_hist_totals)  # for inverse sorting add this at the end [::-1]
 
     # Reshape synthetic data
     # Create matrix of [no. years x no. months x no. samples]
-    synthetic_global = np.zeros([int(768 / n), n, scenarios * sow])
+    synthetic_global = np.zeros([int(len(histData) / n), n, scenarios * sow])
     # Loop through every SOW and reshape to [no. years x no. months]
     for j in range(scenarios * sow):
         synthetic_global[:, :, j] = np.reshape(synthetic[:, j], (int(np.size(synthetic[:, j]) / n), n))
@@ -67,17 +67,17 @@ def plotSDC(synthetic, structure_name):
     p = np.arange(100, -10, -10)
 
     # Calculate synthetic shortage duration curves
-    F_syn = np.empty([int(768 / n), scenarios * sow])
+    F_syn = np.empty([int(len(histData) / n), scenarios * sow])
     F_syn[:] = np.NaN
     for j in range(scenarios * sow):
         F_syn[:, j] = np.sort(synthetic_global_totals[:, j])
 
     # For each percentile of magnitude, calculate the percentile among the experiments ran
     perc_scores = np.zeros_like(F_syn)
-    for m in range(int(768 / n)):
+    for m in range(int(len(histData) / n)):
         perc_scores[m, :] = [stats.percentileofscore(F_syn[m, :], j, 'rank') for j in F_syn[m, :]]
 
-    P = np.arange(1., 768/12 + 1) * 100 / (768/12)
+    P = np.arange(1., len(histData)/12 + 1) * 100 / (len(histData)/12)
 
     ylimit = np.max(F_syn)
     fig, (ax1) = plt.subplots(1, 1, figsize=(14.5, 8))
@@ -126,8 +126,8 @@ else:
     stop = start + count
 
 for i in range(start, stop):
-    histData =  historical.loc[all_IDs[i]].values * 1233.4818 / 1000000
-    synthetic = np.zeros([768, scenarios * sow])
+    histData = historical.loc[all_IDs[i]].values[-768:] * 1233.4818 / 1000000
+    synthetic = np.zeros([len(histData), scenarios * sow])
     for j in range(scenarios):
         path = '../' + design + '/Infofiles/' + all_IDs[i] + '/' + all_IDs[i] + '_info_' + directories[j] + '.txt'
         data = np.loadtxt(path)
@@ -135,6 +135,6 @@ for i in range(start, stop):
             synthetic[:, j * sow:j * sow + sow] = data[:, idx] * 1233.4818 / 1000000
         except ValueError:
             print(all_IDs[i] + '_info_' + directories[j])
-    plotSDC(synthetic, all_IDs[i])
+    plotSDC(synthetic, histData, all_IDs[i])
 
 
