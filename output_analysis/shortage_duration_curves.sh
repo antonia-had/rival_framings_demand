d8c934832fafaae7968c9cd2851bece47a16e00b
antonia-had <ah986@cornell.edu>
2020-10-05 18:08:47 -0400

    edit for uncurtailed scenarios

--- output_analysis/shortage_duration_curves.sh
+++ output_analysis/shortage_duration_curves.sh
@@ -10,4 +10,4 @@
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/rival_framings/bin/activate
-mpirun python3 shortage_duration_curves.py CMIP_curtailment
+mpirun python3 shortage_duration_curves.py CMIP_scenarios
