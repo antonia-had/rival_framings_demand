6e921dd423dd601bdbe8a2cdd5cae84399749877
antonia-had <ah986@cornell.edu>
2020-10-05 22:33:01 -0400

    nodes

--- curtailment_scaling.sh
+++ curtailment_scaling.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="curtailment_scaling"
 #SBATCH --output="curtailment_scaling.out"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=16
 #SBATCH --export=ALL
-#SBATCH -t 10:00:00            # set max wallclock time
+#SBATCH -t 8:00:00            # set max wallclock time
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/rival_framings/bin/activate
