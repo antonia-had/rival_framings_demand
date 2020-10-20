e4dc558646fda71abc7fffba3d46efd983473e6f
antonia-had <ah986@cornell.edu>
2020-10-19 20:51:31 -0400

    rerun debugged realizations

--- submit_CMIP_curtailment.sh
+++ submit_CMIP_curtailment.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=16
 #SBATCH --export=ALL
-#SBATCH -t 10:00:00            # set max wallclock time
+#SBATCH -t 6:00:00            # set max wallclock time
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/rival_framings/bin/activate
