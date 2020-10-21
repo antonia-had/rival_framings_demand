6cd1dd0b6ebcf5f75c5f78611dadd068469b1c26
antonia-had <ah986@cornell.edu>
2020-10-21 16:03:42 -0400

    rerun for missing infofiles

--- output_analysis/infofile_uncurtailed.sh
+++ output_analysis/infofile_uncurtailed.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=10
 #SBATCH --ntasks-per-node=16
 #SBATCH --export=ALL
-#SBATCH -t 20:00:00            # set max wallclock time
+#SBATCH -t 10:00:00            # set max wallclock time
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
 
