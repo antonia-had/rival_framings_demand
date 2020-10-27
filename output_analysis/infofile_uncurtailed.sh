479f887aa4f9814fe8677018aa5c9111b8f53f8f
antonia-had <ah986@cornell.edu>
2020-10-27 16:29:18 -0400

    infofiles for scenarios

--- output_analysis/infofile_uncurtailed.sh
+++ output_analysis/infofile_uncurtailed.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name="infofiles"
 #SBATCH --output="infofiles.out"
 #SBATCH --hint=nomultithread
-#SBATCH --nodes=10
-#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=6
 #SBATCH --export=ALL
-#SBATCH -t 10:00:00            # set max wallclock time
+#SBATCH -t 1:00:00            # set max wallclock time
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
 
 module load python/3.6.9
 source /home/fs02/pmr82_0001/ah986/envs/rival_framings/bin/activate
-mpirun python3 infofile_uncurtailed.py CMIP_curtailment
+mpirun python3 infofile_uncurtailed.py CMIP_scenarios
