2b6409a6043c21f7db8e9265ef513693e4124ab3
antonia-had <ah986@cornell.edu>
2020-10-27 21:41:02 -0400

    infofiles for CMIP_scenarios

--- output_analysis/generate_summary_files.py
+++ output_analysis/generate_summary_files.py
@@ -31,15 +31,15 @@ all_IDs = ['3600687', '7000550', '7200799', '7200645', '3704614', '7202003']#np.
 for i in range(len(all_IDs)):
     ID = all_IDs[i]
     summary_file_path = '../' + design + '/Infofiles/' + ID + '/' + ID + '_all.txt'
-    if os.path.exists(summary_file_path):
-        SYN_short = np.loadtxt(summary_file_path)
-    else:
-        SYN_short = np.zeros([no_months, scenarios*realizations])
-        for j in range(scenarios):
-            infofile_path = '../' + design + '/Infofiles/' + ID + '/' + ID + '_info_' + directories[j] + '.txt'
-            data = np.loadtxt(infofile_path)
-            try:
-                SYN_short[:, j * realizations:j * realizations + realizations] = data[:, idx]
-            except IndexError:
-                print('IndexError ' + ID + '_info_' + directories[j])
-        np.savetxt(summary_file_path, SYN_short)
+    # if os.path.exists(summary_file_path):
+    #     SYN_short = np.loadtxt(summary_file_path)
+    # else:
+    SYN_short = np.zeros([no_months, scenarios*realizations])
+    for j in range(scenarios):
+        infofile_path = '../' + design + '/Infofiles/' + ID + '/' + ID + '_info_' + directories[j] + '.txt'
+        data = np.loadtxt(infofile_path)
+        try:
+            SYN_short[:, j * realizations:j * realizations + realizations] = data[:, idx]
+        except IndexError:
+            print('IndexError ' + ID + '_info_' + directories[j])
+    np.savetxt(summary_file_path, SYN_short)
