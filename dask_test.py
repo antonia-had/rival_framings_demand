7728b1c5252a05bc5a7e19edcbc285ede4c05589
antonia-had <ah986@cornell.edu>
2021-06-24 17:39:26 -0400

    debug daskjobqueue

--- dask_test.py
+++ dask_test.py
@@ -24,9 +24,6 @@ import io
 import numpy as np
 import pandas as pd
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-
-
 sample_number_regex = re.compile(r'_S(\d+)_')
 realization_number_regex = re.compile(r'_(\d+)(?:\.xdd)?$')
 expected_column_sizes = np.asarray([
@@ -65,80 +62,80 @@ def file_manipulator(temporary_path, file_path):
         logging.error(f"Unable to parse sample or realization number from file name: {path.stem}.")
         return False
     np.savetxt('S' + str(sample_number) + '_' + str(realization_number) + '.txt', np.arange(5))
-    # stream will hold CSV of interesting data
-    stream = io.StringIO()
-    # read the file line by line
-    with open(path, 'r') as file:
-        for line in file:
-            # note here that we make two simplifying assumptions:
-            #   - all structure ids of interest start with a digit
-            #   - only lines of data start with a digit
-            if line[0].isdigit() and line[0:expected_column_sizes[0] + 1].strip() in ids_of_interest:
-                if len(line) != expected_line_size:
-                    # unexpected line length; you need to double check the expected column sizes
-                    logging.error(
-                        f"Unexpected line length: {len(line)} instead of {expected_line_size}:\n{line}"
-                    )
-                    return False
-                # split data by character counts
-                data = []
-                position = 0
-                for count in expected_column_sizes:
-                    data.append(line[position:position + count].strip())
-                    # account for single space between columns
-                    position += count + 1
-                if len(data) != expected_column_count:
-                    # unexpected number of columns; you need to double check your data and settings
-                    logging.error(
-                        f"Unexpected column count: {len(data)} instead of {expected_column_count}:\n{line}"
-                    )
-                    return False
-                # only keep non-total rows
-                if not data[month_column].casefold().startswith('tot'):
-                    stream.write(
-                        ','.join(
-                            [data[i] for i in [
-                                id_column,
-                                year_column,
-                                month_column,
-                                demand_column,
-                                shortage_column
-                            ]]
-                        )
-                    )
-                    stream.write('\n')
-    stream.seek(0)
-
-    df = pd.read_csv(
-        stream,
-        header=None,
-        names=[
-            id_column_name,
-            year_column_name,
-            month_column_name,
-            demand_column_name,
-            shortage_column_name
-        ],
-        dtype={
-            id_column_name: id_column_type,
-            year_column_name: year_column_type,
-            month_column_name: month_column_type,
-            demand_column_name: demand_column_type,
-            shortage_column_name: shortage_column_type
-        }
-    )
-
-    stream.close()
-
-    df[sample_column_name] = sample_column_type(sample_number)
-    df[realization_column_name] = realization_column_type(realization_number)
-
-    df.to_parquet(
-        Path(f'{temporary_path}/S{sample_number}_{realization_number}.parquet'),
-        engine='pyarrow',
-        compression='gzip'
-    )
-    np.savetxt('S'+str(sample_number)+'_'+str(realization_number)+'.txt', np.arange(100))
+    # # stream will hold CSV of interesting data
+    # stream = io.StringIO()
+    # # read the file line by line
+    # with open(path, 'r') as file:
+    #     for line in file:
+    #         # note here that we make two simplifying assumptions:
+    #         #   - all structure ids of interest start with a digit
+    #         #   - only lines of data start with a digit
+    #         if line[0].isdigit() and line[0:expected_column_sizes[0] + 1].strip() in ids_of_interest:
+    #             if len(line) != expected_line_size:
+    #                 # unexpected line length; you need to double check the expected column sizes
+    #                 logging.error(
+    #                     f"Unexpected line length: {len(line)} instead of {expected_line_size}:\n{line}"
+    #                 )
+    #                 return False
+    #             # split data by character counts
+    #             data = []
+    #             position = 0
+    #             for count in expected_column_sizes:
+    #                 data.append(line[position:position + count].strip())
+    #                 # account for single space between columns
+    #                 position += count + 1
+    #             if len(data) != expected_column_count:
+    #                 # unexpected number of columns; you need to double check your data and settings
+    #                 logging.error(
+    #                     f"Unexpected column count: {len(data)} instead of {expected_column_count}:\n{line}"
+    #                 )
+    #                 return False
+    #             # only keep non-total rows
+    #             if not data[month_column].casefold().startswith('tot'):
+    #                 stream.write(
+    #                     ','.join(
+    #                         [data[i] for i in [
+    #                             id_column,
+    #                             year_column,
+    #                             month_column,
+    #                             demand_column,
+    #                             shortage_column
+    #                         ]]
+    #                     )
+    #                 )
+    #                 stream.write('\n')
+    # stream.seek(0)
+    #
+    # df = pd.read_csv(
+    #     stream,
+    #     header=None,
+    #     names=[
+    #         id_column_name,
+    #         year_column_name,
+    #         month_column_name,
+    #         demand_column_name,
+    #         shortage_column_name
+    #     ],
+    #     dtype={
+    #         id_column_name: id_column_type,
+    #         year_column_name: year_column_type,
+    #         month_column_name: month_column_type,
+    #         demand_column_name: demand_column_type,
+    #         shortage_column_name: shortage_column_type
+    #     }
+    # )
+    #
+    # stream.close()
+    #
+    # df[sample_column_name] = sample_column_type(sample_number)
+    # df[realization_column_name] = realization_column_type(realization_number)
+    #
+    # df.to_parquet(
+    #     Path(f'{temporary_path}/S{sample_number}_{realization_number}.parquet'),
+    #     engine='pyarrow',
+    #     compression='gzip'
+    # )
+    # np.savetxt('S'+str(sample_number)+'_'+str(realization_number)+'.txt', np.arange(100))
     return
 
 L=client.map(file_manipulator, './output_test', './scenarios/S1_1/*.xdd')
