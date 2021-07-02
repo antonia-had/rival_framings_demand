18b50de1c3a2aaf32d97f598e63d92b48a9abfc2
antonia-had <ah986@cornell.edu>
2021-07-02 00:26:37 -0400

    sleep

--- submit_all_runs.sh
+++ submit_all_runs.sh
@@ -5,6 +5,6 @@ end_vals=($(seq 50 50 600))
 batches=($(seq 1 12))
 
 for i in "${!batches[@]}"; do
-  sbatch --job-name=batch_${batches[$i]} --output=/outputs/batch_${batches[$i]}.out --error=/errors/batch_${batches[$i]}.err curtailment_scaling_comet.sh ${start_vals[$i]} ${end_vals[$i]}
+  sbatch --job-name=batch_${batches[$i]} --output=./outputs/batch_${batches[$i]}.out --error=./errors/batch_${batches[$i]}.err curtailment_scaling_comet.sh ${start_vals[$i]} ${end_vals[$i]}
   sleep 0.5
 done
