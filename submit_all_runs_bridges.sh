0130d194241f8de2c7787fd2ebfdf0d40051c576
antonia-had <ah986@cornell.edu>
2021-07-02 12:36:05 -0400

    batch indexing

--- submit_all_runs_bridges.sh
+++ submit_all_runs_bridges.sh
@@ -5,9 +5,9 @@ end_vals=($(seq 50 50 600))
 batches=($(seq 4 6))
 
 for i in "${batches[@]}"; do
-  sbatch --job-name=batch_${batches[$i]} \
-  --output=./outputs/batch_${batches[$i]}.out \
-  --error=./errors/batch_${batches[$i]}.err \
-  curtailment_scaling_bridges.sh ${start_vals[$i]} ${end_vals[$i]} ${batches[$i]}
+  sbatch --job-name=batch_$i} \
+  --output=./outputs/batch_$i]}.out \
+  --error=./errors/batch_$i}.err \
+  curtailment_scaling_bridges.sh ${start_vals[$i]} ${end_vals[$i]} $i}
   sleep 0.5
 done
