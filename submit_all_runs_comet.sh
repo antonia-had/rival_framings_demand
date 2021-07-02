0d3776008373594cc36fc4ca37e7caf1e7936e70
antonia-had <ah986@cornell.edu>
2021-07-02 12:37:24 -0400

    batch indexing

--- submit_all_runs_comet.sh
+++ submit_all_runs_comet.sh
@@ -5,9 +5,9 @@ end_vals=($(seq 50 50 600))
 batches=($(seq 1 3))
 
 for i in "${batches[@]}"; do
-  sbatch --job-name=batch_$i} \
-  --output=./outputs/batch_$i}.out \
-  --error=./errors/batch_$i}.err \
-  curtailment_scaling_comet.sh ${start_vals[$i]} ${end_vals[$i]} $i}
+  sbatch --job-name=batch_$i \
+  --output=./outputs/batch_$i.out \
+  --error=./errors/batch_$i.err \
+  curtailment_scaling_comet.sh ${start_vals[$i]} ${end_vals[$i]} $i
   sleep 0.5
 done
