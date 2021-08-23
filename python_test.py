724dd236899974fe182dcfba426f9a5a6b23a0aa
antonia-had <ah986@cornell.edu>
2021-08-23 19:13:23 -0400

    testing bash

--- python_test.py
+++ python_test.py
@@ -2,6 +2,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract monthly and annual flows per realization.')
+    parser.add_argument('p', type=str)
     parser.add_argument('i', type=int,
                         help='scenario number')
     parser.add_argument('j', type=int,
@@ -9,4 +10,4 @@ if __name__ == '__main__':
     parser.add_argument('k', type=int,
                         help='realization number')
     args = parser.parse_args()
-    print("this script would run " + str(args.i) + " " + str(args.j) + " " + str(args.k))
+    print("core " + args.p + " would run " + str(args.i) + " " + str(args.j) + " " + str(args.k))
