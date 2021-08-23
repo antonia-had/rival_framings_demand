112cd0dd8469c6cfe3af27172b2be314232a90a8
antonia-had <ah986@cornell.edu>
2021-08-23 17:55:08 -0400

    testing bash

--- curtailment_scaling_expanse.sh
+++ curtailment_scaling_expanse.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
 
-module purge
+module load cpu/0.15.4
 module load parallel
 #module load anaconda3
 #source activate /home/ah986/.conda/envs/adaptive_demands
