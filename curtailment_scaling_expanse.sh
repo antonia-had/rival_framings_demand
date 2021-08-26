06e5119e60b353ff74a6992aa5cee5e1b65fc591
antonia-had <ah986@cornell.edu>
2021-08-26 12:04:52 -0400

    testing cpus per task

--- curtailment_scaling_expanse.sh
+++ curtailment_scaling_expanse.sh
@@ -2,9 +2,10 @@
 #SBATCH --partition=shared
 #SBATCH --account=TG-MCA08X018
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=100
-#SBATCH --mem=200G
-#SBATCH --time=2:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
+#SBATCH --time=1:00:00
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
 #SBATCH --export=ALL
@@ -17,7 +18,7 @@ source activate /home/ah986/.conda/envs/adaptive_demands
 
 # This specifies the options used to run srun. The "-N1 -n1" options are
 # used to allocates a single core to each task.
-srun="srun --export=all --exclusive -N1 -n1"
+srun="srun --export=all"
 # This specifies the options used to run GNU parallel:
 #
 #   --delay of 0.2 prevents overloading the controlling node.
@@ -27,7 +28,7 @@ srun="srun --export=all --exclusive -N1 -n1"
 #   The combination of --joblog and --resume create a task log that
 #   can be used to monitor progress.
 #
-parallel="parallel --delay 0.2 -j $SLURM_NTASKS --joblog curtailment_scaling_$3.log --resume"
+parallel="parallel --delay 0.2 -j $SLURM_CPUS_PER_TASK --joblog curtailment_scaling_$3.log --resume"
 echo "Submitting samples $1 to $2"
 vals=($(seq $1 $2))
-$srun $parallel "python3 curtailment_scaling.py" ::: {1..100} ::: "${vals[@]}"
+$srun $parallel "python3 curtailment_scaling.py" ::: {1..4} ::: "${vals[@]}"
