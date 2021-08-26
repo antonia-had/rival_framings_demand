b84c182d0100b1c2de36f786c1c86d01d22400aa
antonia-had <ah986@cornell.edu>
2021-08-26 16:58:07 -0400

    fixed bash sequencing

--- submit_all_runs_comet.sh
+++ submit_all_runs_comet.sh
@@ -2,9 +2,9 @@
 
 start_vals=($(seq 1 50 551))
 end_vals=($(seq 50 50 600))
-batches=($(seq 1 3))
+$(seq 7 8)
 
-for i in "${batches[@]}"; do
+for i in $(seq 0 11); do
   sbatch --job-name=batch_$i \
   --output=./outputs/batch_$i.out \
   --error=./errors/batch_$i.err \
