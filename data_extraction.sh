737b6571b6ccc165cbf3690f90776c69a003eab5
antonia-had <ah986@cornell.edu>
2021-09-09 16:54:59 -0400

    test submission script

--- data_extraction.sh
+++ data_extraction.sh
@@ -9,5 +9,4 @@
 # SBATCH --job-name xdd_to_parquet
 
 source /home/fs02/pmr82_0001/ah986/rival_framings/bin/activate
-
-python3 statemod_data_extraction.py -i ./ids.txt
+ibrun python3 statemod_data_extraction.py -i ./ids.txt
