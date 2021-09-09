64b6df779591769b25dc4190bad592fe7f44b824
antonia-had <ah986@cornell.edu>
2021-09-09 17:15:40 -0400

    test submission script

--- data_extraction.sh
+++ data_extraction.sh
@@ -5,7 +5,6 @@
 # SBATCH --cpus-per-task=1
 # SBATCH -p normal
 # SBATCH -t 03:00:00
-# SBATCH --exclusive
 # SBATCH --job-name xdd_to_parquet
 
 source /home/fs02/pmr82_0001/ah986/rival_framings/bin/activate
