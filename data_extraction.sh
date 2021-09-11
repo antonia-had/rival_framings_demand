b637210a437423ae2c01b675723c11b4c22497b3
antonia-had <ah986@cornell.edu>
2021-09-10 22:24:27 -0400

    testing mpi executor

--- data_extraction.sh
+++ data_extraction.sh
@@ -9,4 +9,4 @@
 
 
 source /home/fs02/pmr82_0001/ah986/rival_framings/bin/activate
-mpiexec -n 1 -usize 20 python3 statemod_data_extraction.py -i ./ids.txt
+MPIEXEC_UNIVERSE_SIZE=20 mpiexec -n 1 python3 statemod_data_extraction.py -i ./ids.txt
