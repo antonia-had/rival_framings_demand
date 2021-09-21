ab11fce7aa41dca95a4441f6180cae91b934766f
antonia-had <ah986@cornell.edu>
2021-09-20 21:09:45 -0400

    new mpi division of labor

--- data_extraction.sh
+++ data_extraction.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#SBATCH --nodes=2
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=80
 #SBATCH -p normal
-#SBATCH -t 0:30:00
+#SBATCH -t 3:00:00
 #SBATCH --export=ALL
-
+#SBATCH --mail-user=ah986@cornell.edu
+#SBATCH --mail-type=ALL
 
 source /home/fs02/pmr82_0001/ah986/rival_framings/bin/activate
 mpirun python3 data_extraction.py
