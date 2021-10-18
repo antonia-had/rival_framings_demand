f87c18149013436fb9ec8dce17dd101eb795f705
antonia-had <ah986@cornell.edu>
2021-10-18 19:37:07 -0400

    rule data extraction

--- rule_data_extraction.py
+++ rule_data_extraction.py
@@ -9,6 +9,7 @@ def create_file_per_rule(sow, rule):
     print(f'working on SOW {sow} rule {rule}')
     df = dd.read_parquet(Path(f'{statemod_outputs}/**/S{sow}_*_{rule}.parquet'),
                          engine='pyarrow-dataset').compute()
+    print(f'saving {rule_outputs}/Rule_{rule}/S{sow}_{rule}.parquet')
     df.to_parquet(Path(f'{rule_outputs}/Rule_{rule}/S{sow}_{rule}.parquet'), engine='pyarrow',
                   compression='gzip')
     return True
