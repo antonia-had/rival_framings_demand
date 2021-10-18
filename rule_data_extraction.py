61e1fe1c68a62a712218cfe57da448e02750d6e1
antonia-had <ah986@cornell.edu>
2021-10-18 18:37:03 -0400

    rule data extraction

--- rule_data_extraction.py
+++ rule_data_extraction.py
@@ -1,6 +1,8 @@
 import dask.dataframe as dd
 from pathlib import Path
 import argparse
+from mpi4py import MPI
+
 
 statemod_outputs = './xdd_parquet'
 rule_outputs = './rules_parquet'
@@ -10,9 +12,31 @@ def create_file_per_rule(rule):
                          engine='pyarrow-dataset').compute()
     df.to_parquet(Path(f'{rule_outputs}/{rule}.parquet'), engine='pyarrow',
                   compression='gzip')
+    return True
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create summary parquet per rule')
-    parser.add_argument('rule', type=str)
-    args = parser.parse_args()
-    create_file_per_rule(args.rule)
+# Begin parallel simulation
+comm = MPI.COMM_WORLD
+
+# Get the number of processors and the rank of processors
+rank = comm.rank
+nprocs = comm.size
+
+# Divide all SOWs to available cores
+
+# Determine the chunk which each processor will need to do
+count = int(600/nprocs)
+remainder = 600 % nprocs
+
+# Use the processor rank to determine the chunk of work each processor will do
+if rank < remainder:
+    start = rank * (count+1)
+    stop = start + count + 1
+else:
+    start = remainder * (count+1) + (rank-remainder) * count
+    stop = start + count
+
+print("Process " + str(rank) + " working on rules from " + str(start) + " to " + str(stop))
+for k in range(start, stop):
+    rule_success = create_file_per_rule(k)
+    if not rule_outputs:
+        print(f'Failed to create file for rule {k}')
--- rule_data_extraction.sh
+++ rule_data_extraction.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-#SBATCH --nodes=1                   # Use one node
-#SBATCH --ntasks-per-node=1                 # Run a single task
+#SBATCH --nodes=15
+#SBATCH --ntasks-per-node=5
+#SBATCH -t 2:00:00
+#SBATCH --export=ALL
 #SBATCH --exclusive
-#SBATCH --time 10:00:00
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-15                # Array range
 
 source /home/fs02/pmr82_0001/ah986/rival_framings/bin/activate
-
-#Set the number of tasks that each SLURM task should do
-PER_TASK=40
-
-# Calculate the starting and ending values for this task based
-# on the SLURM task and the number of runs per task.
-START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * $PER_TASK + 1 ))
-END_NUM=$(( $SLURM_ARRAY_TASK_ID * $PER_TASK ))
-
-# Run the loop of runs for this task.
-for (( rule=$START_NUM; rule<=$END_NUM; rule++ )); do
-  echo This is SLURM task $SLURM_ARRAY_TASK_ID, run extraction for $rule
-  srun python3 rule_data_extraction.py $rule
-done
+mpirun python3 rule_data_extraction.py
