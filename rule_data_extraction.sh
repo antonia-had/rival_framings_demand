acd3a8d26537ed74ee121f8a7810452051e80890
antonia-had <ah986@cornell.edu>
2021-10-18 18:30:05 -0400

    rule data extraction

--- rule_data_extraction.sh
+++ rule_data_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1                   # Use one node
 #SBATCH --ntasks-per-node=1                 # Run a single task
-#SBATCH --exclusive=user
+#SBATCH --exclusive
 #SBATCH --time 10:00:00
 #SBATCH --mail-user=ah986@cornell.edu
 #SBATCH --mail-type=ALL
