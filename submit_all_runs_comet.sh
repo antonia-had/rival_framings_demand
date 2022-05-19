aa0fbbbbcfca2bcbb5f61bea1a0d5000ff252d8c
antonia-had <31770297+antonia-had@users.noreply.github.com>
2022-05-19 11:56:56 -0400

    Update submit_all_runs_comet.sh

--- submit_all_runs_comet.sh
+++ submit_all_runs_comet.sh
@@ -3,7 +3,7 @@
 start_vals=($(seq 1 50 551))
 end_vals=($(seq 50 50 600))
 
-for i in $(seq 6 7); do
+for i in $(seq 0 11); do
   sbatch --job-name=batch_$i \
   --output=./outputs/batch_$i.out \
   --error=./errors/batch_$i.err \
